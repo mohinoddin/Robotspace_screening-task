ae3e3ec2715ede311b7a5c77b1a5487a3f933515
mohinoddin <mohinoddin80@gmail.com>
2022-10-07 20:58:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log('hii')
+
+const timer = document.getElementById('timer')
+var h1txt=timer.innerText
+const startBtn = document.getElementById('startbtn')
+const stopBtn = document.getElementById('stopbtn')
+let counter=0,stopTime=true;
+
+
+  
+const  startTimer=()=>{
+    if(stopTime==true){
+        stopTime=false
+        startBtn.style.background='yellow'
+        timerCycle()
+    }
+}
+
+
+const stopTimer=()=>{
+    if(stopTime==false){
+        stopTime=true
+        startBtn.style.background='#08df1e'
+        stopBtn.style.background='yellow'
+    }
+}
+const timerCycle=()=>{
+    if(stopTime==false){
+        counter=parseInt(counter)
+        counter=counter+1
+        timer.innerHTML= h1txt + " : " + counter;
+        setTimeout("timerCycle()",1000)
+    }
+}
+
+function resetTimer(){
+    
+    stopTime=true
+    startBtn.style.background='#08df1e'
+    stopBtn.style.background='#08df1e'
+    counter=0
+    timer.innerHTML= h1txt
+  
+}
